d27a650f0342bbab2f15aedf4c8f89b6821ac7b0
Jagajeet Kuppala <33180965+Jagajeet-kuppala@users.noreply.github.com>
2019-06-11 09:16:38 +0530

    Create SignOfNumber.c

---
+++ SignOfNumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+ 	long long int num;
+	scanf("%d",&num);
+	if(num==0)	printf("Zero");
+	else if(num<0)	printf("Negative");
+	else printf("Positive");
+  return 0;
+}
